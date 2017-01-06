099e36a78e3f768af3cbb213096b427f1e9d2ba4
Mike <mikegol@live.com>
2017-01-05 21:52:39 -0800

    alpha 1 of the text-based game

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-﻿console.log('Hello world');
+﻿import {MinesweeperTextBasedGame} from "./TextBased"
+
+// run Minesweeper
+//
+console.log('Welcome to Minesweeper!');
+let boardSize = 10;
+let numberOfMines = 5;
+console.log('Board size: ' + boardSize);
+console.log('# of mines: ' + numberOfMines);
+let game = new MinesweeperTextBasedGame();
+game.InitGame(boardSize, numberOfMines);
+game.PlayGame();
