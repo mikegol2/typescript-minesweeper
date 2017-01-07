96af1cc380046fad92d5368ca7ad5e55c86e4ba1
Mike <mikegol@live.com>
2017-01-07 12:37:32 -0800

    starting work on web game

---
+++ WebBased.ts
@@ -0,0 +1,43 @@
+﻿// interface JQuery {
+//     fadeIn(): JQuery;
+//     fadeOut(): JQuery;
+//     focus(): JQuery;
+//     html(): string;
+//     html(val: string): JQuery;
+//     show(): JQuery;
+//     addClass(className: string): JQuery;
+//     removeClass(className: string): JQuery;
+//     append(el: HTMLElement): JQuery;
+//     val(): string;
+//     val(value: string): JQuery;
+//     attr(attrName: string): string;
+// }
+// declare var $: {
+//     (el: HTMLElement): JQuery;
+//     (selector: string): JQuery;
+//     (readyCallback: () => void ): JQuery;
+// };
+
+function DoSomething() {
+    alert('hey!');
+}
+
+
+// $(document).ready(function()
+// {
+// let field = document.getElementById('#field');
+// field.innerHTML = 'hey!';
+// }
+
+
+//class Greeter {
+//    constructor(public greeting: string) { }
+//    greet() {
+//        return "<h1>" + this.greeting + "</h1>";
+//    }
+//};
+
+//var greeter = new Greeter("Hello, world!");
+
+//document.body.innerHTML = greeter.greet();
+
